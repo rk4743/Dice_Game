c22aa036dba85af9d5989cc8dafa7aa0e2330ca3
Ritik Katiyar <ritikkatiyar90@gmail.com>
2024-01-31 23:36:56 +0530

    Added necessary files

---
+++ Dice Game/index.js
@@ -0,0 +1,31 @@
+//Player1
+
+var randomNumber1 = Math.floor (Math.random() * 6) + 1 ;
+
+var randomImage1 = "dice" + randomNumber1 + ".png" ;
+
+var randomImageSource1 = "images/" + randomImage1 ;
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
+
+//Player2
+
+var randomNumber2 = Math.floor (Math.random() * 6) + 1 ;
+
+var randomImage2 = "dice" + randomNumber2 + ".png" ;
+
+var randomImageSource2 = "images/" + randomImage2 ;
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+//The Winner!!!!!
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="🚩 Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="🚩 Draw!";
+}
